7d3d7fe39345af3d6d04961ea04ae0bfee28bc07
shibudaniel <47894492+shibuadaniel@users.noreply.github.com>
2020-10-09 15:44:21 -0400

    Initial Commit

---
+++ Landmark-Pass2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmark-Pass2
+//
+//  Created by SHIBU DANIEL on 10/9/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Landmark-Pass2/Landmark_Pass2App.swift
@@ -0,0 +1,17 @@
+//
+//  Landmark_Pass2App.swift
+//  Landmark-Pass2
+//
+//  Created by SHIBU DANIEL on 10/9/20.
+//
+
+import SwiftUI
+
+@main
+struct Landmark_Pass2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
